cacbcdc674153d99db1c7c044d19b6ecb9ca9b10
Mericic <aymericboi@gmail.com>
2018-08-19 12:14:52 +0200

    authentification

---
+++ app/Contenu.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contenu extends Model
+{
+    protected $primaryKey = 'id_Contenu';
+
+    public function getEvent(int $type) :int{
+
+    }
+
+}
