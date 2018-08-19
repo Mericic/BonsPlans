8b0ba85346005ec572d1db978dec90bc77d6a6c3
MaxenceMGR <maxencemgr@laposte.net>
2018-08-20 00:35:16 +0200

    placement des point sur la map avec les contenu de la base

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -7,8 +7,14 @@ use Illuminate\Http\Request;
 
 class MapsController extends Controller
 {
-    public function getInfoContenu($idContenu){
-        $Contenu = Contenu::where('contenu.id_Contenu', '=', $idContenu)
+    public function getContenuStart($latitude, $longitude){
+        $Contenu = Contenu::join('users', 'contenus.id_User', '=', 'users.id')
+            ->select('users.pseudo', 'contenus.*')
+            ->where('contenus.CoordonneesX', '<=', $latitude + 0.005)
+            ->where('contenus.CoordonneesY', '<=', $longitude + 0.005)
+            ->where('contenus.CoordonneesX', '>=', $latitude - 0.005)
+            ->where('contenus.CoordonneesY', '>=', $longitude - 0.005)
+            ->distinct()
             ->get();
         return $Contenu;
     }
