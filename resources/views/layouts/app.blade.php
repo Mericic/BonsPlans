32155c2344ca6d4982d99c31110185887836e452
Mericic <aymericboi@gmail.com>
2018-08-20 11:34:47 +0200

    connexion deconnexion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,18 +41,100 @@
             <a style="color: white" class="nav-link" href="{{ route('register') }}">Inscription</a>
         </li>
         <li class="nav-item">
-            <a style="color: white" class="nav-link" href="#">Connexion</a>
+            <a style="color: white" class="nav-link" data-toggle="modal" data-target="#ConnexionModal" id="boutonModal">Connexion</a>
         </li>
         @else
             <li class="nav-item">
                 <a style="color: white" class="nav-link" href="{{ route('profil', ['pseudo'=>Auth::user()->pseudo]) }}">Mon Profil</a>
+            </li>
+            <li class="nav-item">
+                <a style="color: white" class="nav-link" onclick="$('#formdeco').submit()">Deconnexion</a>
+                <form method="post" action="{{  route('logout') }}" id="formdeco">@csrf</form>
             </li>
         @endif
     </ul>
 </nav>
 
 @yield('content')
+
+
+<div class="modal fade" id="ConnexionModal" tabindex="-1" role="dialog" aria-labelledby="ConnexionModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ConnexionModalLabel">Connexion</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @csrf
+
+                    <div class="form-group row">
+                        <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Remember Me') }}
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Login') }}
+                            </button>
+
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                <button type="button" class="btn btn-primary" onclick="$('#repriseForm').submit()">Enregistrer</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script src="\js\smoothJump.js"></script>
 <script src="\js\infiniteScroll.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 </html>
