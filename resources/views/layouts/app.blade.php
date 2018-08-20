266f4558a1e262eace3dd0e292149b5c4889650b
Mericic <aymericboi@gmail.com>
2018-08-20 23:57:40 +0200

    franssè

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,7 +74,7 @@
                     @csrf
 
                     <div class="form-group row">
-                        <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Adresse email') }}</label>
 
                         <div class="col-md-6">
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -88,7 +88,7 @@
                     </div>
 
                     <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Môôôôôt de pââââsse') }}</label>
 
                         <div class="col-md-6">
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -107,7 +107,7 @@
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                 <label class="form-check-label" for="remember">
-                                    {{ __('Remember Me') }}
+                                    {{ __('Se souvenir de moi') }}
                                 </label>
                             </div>
                         </div>
@@ -116,20 +116,17 @@
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
                             <button type="submit" class="btn btn-primary">
-                                {{ __('Login') }}
+                                {{ __('Connexion') }}
                             </button>
 
                             <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Forgot Your Password?') }}
+                                {{ __('Mot de passe oublié ?') }}
                             </a>
                         </div>
                     </div>
                 </form>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                <button type="button" class="btn btn-primary" onclick="$('#repriseForm').submit()">Enregistrer</button>
-            </div>
+
         </div>
     </div>
 </div>
