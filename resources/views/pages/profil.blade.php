d001f617f1c37351e5f2e1f0f109dce34acdd155
antoinedrt <antoine.darinot@gmail.com>
2018-08-20 18:41:09 +0200

    profile css html

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -3,9 +3,201 @@
 
 @section('content')
 
-    <div>
-        <p>coucou</p>
+<div id="profile-container">
+    <div id="profile-wrapper">
+        <div id="user-info-container">
+            <div id="profile-picture"><img src="#"></div>
+            <div id="username">USERNAME <br> Name</div>
+            <div id="user-info">User info <br> user info</div>
+        </div>
+        <div id="fav-categories">
+            <div class="profil-progress">
+                <p>Categorie 1</p>
+                <div class="progress">
+                    <div class="progress-bar .fav" role="progressbar" style="width: 95%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="profil-progress">
+                <p>Categorie 2</p>
+                <div class="progress">
+                    <div class="progress-bar .fav" role="progressbar" style="width: 65%" aria-valuenow="3" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="profil-progress">
+                <p>Categorie 3</p>
+                <div class="progress">
+                    <div class="progress-bar .fav" role="progressbar" style="width: 55%" aria-valuenow="4" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="profil-progress">
+                <p>Categorie 4</p>
+                <div class="progress">
+                    <div class="progress-bar .fav" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+        <div id="com-historique">
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="profil-com">
+                <p class="com-title">Com-Title</p>
+                <div class="progress .com-bar">
+                    <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                <p class="com-main">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+        <div id="participation">
+            <div class="participation-div">
+                <p class="participation-title">Participation-Title</p>
+                <div class="participation-info"> 
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                </div>
+            </div>
+            <div class="participation-div">
+                <p class="participation-title">Participation-Title</p>
+                <div class="participation-info"> 
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                </div>
+            </div>
+            <div class="participation-div">
+                <p class="participation-title">Participation-Title</p>
+                <div class="participation-info"> 
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                </div>
+            </div>
+            <div class="participation-div">
+                <p class="participation-title">Participation-Title</p>
+                <div class="participation-info"> 
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="creation">
+            <div class="participation-div">
+                <p class="participation-title">Creation-Title</p>
+                <div class="participation-info"> 
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                    <div class="progress .participation-bar">
+                        <div class="progress-bar" role="progressbar" style="width: 45%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="100"><p>Categorie</p></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
+<script>
+    var participation = document.getElementById('participation');
+    var participationDivs = document.getElementsByClassName('participation-div');
+    var participationInfo = document.getElementsByClassName('participation-info');
+    var height = participation.clientHeight;
+    var h = height.toString() + 'px';
+    for (i = 0; i < participationDivs.length; i++){
+        participationDivs[i].style.height = h;
+    }
+    for (i = 0; i < participationInfo.length; i++){
+        participationInfo[i].style.height = h;
+    }
+</script>
 
 
 @endsection
