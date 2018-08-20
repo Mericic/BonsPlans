6a8b3e019b5caff22ed062e7a66741d9ac5b1651
MaxenceMGR <maxencemgr@laposte.net>
2018-08-20 19:52:16 +0200

    ajout dynamique des point sur la carte en fonction du zoom + fix

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -18,6 +18,46 @@ class MapsController extends Controller
             ->get();
         return $Contenu;
     }
+    public function getContenuZoom($lvl, $latitude, $longitude){
+        switch ($lvl) {
+            case "512":
+                $zoom = 0.7;
+                break;
+            case "1024":
+                $zoom = 0.35;
+                break;
+            case "2048":
+                $zoom = 0.2;
+                break;
+            case "4096":
+                $zoom = 0.4;
+                break;
+            case "8192":
+                $zoom = 0.05;
+                break;
+            case "16384":
+                $zoom = 0.012;
+                break;
+            case "32768":
+                $zoom = 0.01;
+                break;
+            case "65536":
+                $zoom = 0.005;
+                break;
+            case "131072":
+                $zoom = 0.00075;
+                break;
+        }
+        $Contenu = Contenu::join('users', 'contenus.id_User', '=', 'users.id')
+            ->select('users.pseudo', 'contenus.*')
+            ->where('contenus.CoordonneesX', '<=', $latitude + $zoom)
+            ->where('contenus.CoordonneesY', '<=', $longitude + $zoom)
+            ->where('contenus.CoordonneesX', '>=', $latitude - $zoom)
+            ->where('contenus.CoordonneesY', '>=', $longitude - $zoom)
+            ->distinct()
+            ->get();
+        return $Contenu;
+    }
 
     public function doCircle($idContenu){
 
